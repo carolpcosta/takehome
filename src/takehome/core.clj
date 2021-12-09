6e5d88ccbd70fb469b7238debf11f3c832c0622b
Carolline Costa <carolline.costa@useargo.com>
2021-12-08 22:15:36 -0300

    Adding methods of access (patriota, premium and mecenas).

--- src/takehome/core.clj
+++ src/takehome/core.clj
@@ -1,9 +1,41 @@
 (ns takehome.core
   (:require [java-time :as time]))
 
+(defn patriota-access? [object purchase]
+  (or 
+   (= (:type object) :series)
+   (= (:type object) :podcast)
+   (= (:type object) :debate)
+   (and (= (:type object) :interview)
+        (time/before? (:subscription-start purchase)
+                      (:released-at object)
+                      (:subscription-end purchase)))))
+
+(defn premium-access? [object purchase]
+  (or 
+   (= (:type object) :series)
+   (= (:type object) :podcast)
+   (= (:type object) :debate)
+   (= (:type object) :interview)
+   (and (= (:type object) :course)
+        (time/before? (:subscription-start purchase)
+                      (:released-at object)
+                      (:subscription-end purchase)))))
+
+(defn mecenas-access? [object purchase]
+  (or 
+   (= (:type object) :series)
+   (= (:type object) :podcast)
+   (= (:type object) :debate)
+   (= (:type object) :interview)
+   (= (:type object) :course)
+   (and (= (:type object) :patron)
+        (time/before? (:subscription-start purchase)
+                      (:released-at object)
+                      (:subscription-end purchase)))))
+
 (defn can-access? [object purchase]
-  (if (= (:type object) :movie)
-    (and (= (:type purchase) :patriota)
-         (time/before? (:subscription-start purchase)
-                       (:released-at object)
-                       (:subscription-end purchase)))))
+  (cond
+    (= (:type purchase) :patriota) (patriota-access? object purchase)
+    (= (:type purchase) :premium) (premium-access? object  purchase)
+    (= (:type purchase) :mecenas) (mecenas-access? object purchase)))
